1332bcc89115c5119b776d67f48241f6946c086a
Charles Jackson <cbj170230@utdallas.edu>
2021-01-12 19:42:15 -0600

    Use symbols instead of keywords for export

--- package.lisp
+++ package.lisp
@@ -1,15 +1,16 @@
 (defpackage hunchenissr
   (:use #:cl #:plump #:issr-core)
-  (:export :clients
-           :on-connect-hook
-           :on-disconnect-hook
-           :*ws-port*
-           :*socket*
-           :*first-time*
-           :*id*
-           :define-easy-handler
-           :start
-           :stop
-           :redirect
-           :rr))
+  (:export
+   -clients-
+   -on-connect-hook-
+   -on-disconnect-hook-
+   *ws-port*
+   *socket*
+   *first-time*
+   *id*
+   define-easy-handler
+   start
+   stop
+   redirect
+   rr))
 
