1b386ef0a560ccdfa1441b6a4401debae545e8e0
Charles <charles.b.jackson@protonmail.com>
2021-08-30 22:55:16 -0500

    Start websocket and http servers with start function
    Obviously stop them with the stop function too

--- main.lisp
+++ main.lisp
@@ -1,5 +1,8 @@
 (in-package #:issr)
 
+(defvar *ws-server* nil)
+(defvar *ht-server* nil)
+
 (defun start (config
               &aux (config
                     (cond ((stringp config) (read-config config))
@@ -10,4 +13,27 @@
     :port (config-port config)
     :ssl (config-ssl config)
     :destinator (make-destinator config)
-    :response-filter #'add-ids-and-js-to-html)))
+    :response-filter #'add-ids-and-js-to-html))
+  (multiple-value-bind (host port)
+      (destination-parts (config-application-destination config))
+    (pws:define-resource "/-issr"
+      :message (make-ws-message host port)
+      :error #'ws-error))
+  (setq *ws-server*
+        (pws:server
+         (config-websocket-port config)
+         :multi-thread))
+  (setq *ht-server*
+        (hunchentoot:start
+         (make-instance
+          'easy-acceptor
+          :port (config-http-port config)
+          :document-root "resources/"))))
+
+(defun stop ()
+  (yxorp:stop)
+  (when (and (bt:threadp *ws-server*)
+             (bt:thread-alive-p *ws-server*))
+    (pws:server-close *ws-server*))
+  (when *ht-server*
+    (hunchentoot:stop *ht-server*)))
