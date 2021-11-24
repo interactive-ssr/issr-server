c92c6a46bcbe3d784cdf323eeb51d738dcfb6410
Charles <charles.b.jackson@protonmail.com>
2021-11-23 21:10:11 -0600

    Try to reconnect to redis

--- src/hooks.lisp
+++ src/hooks.lisp
@@ -23,22 +23,27 @@
    (lambda ()
      (block exit
        (loop
-         (handler-case 
-             (redis:with-connection (:host redis-host :port redis-port :auth redis-pass)
-               (red:subscribe "issr-rr")
-               (loop
-                 (let ((message (redis:expect :anything)))
-                   (bt:make-thread
-                    (lambda ()
-                      (match (jojo:parse (third message) :as :alist)
-                        ((guard (list uuid client-id args)
-                                (string= uuid (server-uuid)))
-                         (redis:with-connection (:host redis-host :port redis-port :auth redis-pass)
-                           (some-> client-id
-                             get-id-client
-                             (rr host port show-errors args))))))
-                    :name "re-rendering"))))
-           (end-of-file () (return-from exit))))))
+         (block continue
+           (handler-case
+               (redis:with-connection (:host redis-host :port redis-port :auth redis-pass)
+                 (red:subscribe "issr-rr")
+                 (loop
+                   (let ((message (redis:expect :anything)))
+                     (bt:make-thread
+                      (lambda ()
+                        (match (jojo:parse (third message) :as :alist)
+                          ((guard (list uuid client-id args)
+                                  (string= uuid (server-uuid)))
+                           (redis:with-connection (:host redis-host :port redis-port :auth redis-pass)
+                             (some-> client-id
+                               get-id-client
+                               (rr host port show-errors args))))))
+                      :name "re-rendering"))))
+             (end-of-file () (return-from exit))
+             (usocket:connection-refused-error ()
+               (sleep 2)
+               (format *debug-io* "Trying to connect to redis.~%")
+               (return-from continue)))))))
    :name *hook-listener-name*))
 
 (defun stop-hook-listener ()
