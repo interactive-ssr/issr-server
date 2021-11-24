11b3182ca81fea20f0d1e8119d293ed731a28885
Charles <charles.b.jackson@protonmail.com>
2021-11-23 21:07:54 -0600

    Process environment values in default config

--- src/default-config.lisp
+++ src/default-config.lisp
@@ -1,12 +1,16 @@
 (let ((default (config)))
   (config
    :port
-   (env-or "ISSR_PORT" (config-port default))
-   :show-errors-to-client
-   (env-or "ISSR_SHOW_ERRORS" (config-show-errors default))
+   (env-or "ISSR_PORT" (config-port default) env
+           (read-from-string env))
+   :show-errors
+   (env-or "ISSR_SHOW_ERRORS" (config-show-errors default) env
+           (if (str:blankp env) nil t))
    :application-destination
-   (env-or "ISSR_APPLICATION" (config-application-destination default))
-   :ssl (when (some (compose 'not 'str:blankp)
+   (env-or "ISSR_APPLICATION" (config-application-destination default) env
+           (handler-case (parse-integer env)
+             (parse-error () env)))
+   :ssl (when (some (alexandria:compose 'not 'str:blankp)
                     (map 'list 'uiop:getenv
                          (list "ISSR_SSL_CERT"
                                "ISSR_SSL_KEY"
@@ -25,14 +29,21 @@
                    (yxorp::ssl-config-password (config-ssl default)))
            :redirect-port
            (env-or "ISSR_SSL_REDIRECT_PORT"
-                   (yxorp::ssl-config-redirect-port (config-ssl default)))
+                   (yxorp::ssl-config-redirect-port (config-ssl default))
+                   env
+                   (parse-integer env))
            :redirect-to
            (env-or "ISSR_SSL_REDIRECT_TO"
-                   (yxorp::ssl-config-redirect-to (config-ssl default)))))
+                   (yxorp::ssl-config-redirect-to (config-ssl default))
+                   env
+                   (parse-integer env))))
    :redis (redis-config
            :destination
            (env-or "ISSR_REDIS_DESTINATION"
-                   (redis-config-destination (config-redis default)))
+                   (redis-config-destination (config-redis default))
+                   env
+                   (handler-case (parse-integer env)
+                     (parse-error () env)))
            :password
            (env-or "ISSR_REDIS_PASSWORD"
                    (redis-config-password (config-redis default))))))
