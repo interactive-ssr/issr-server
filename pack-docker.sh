ad75c0312882b86b6166c7b875deb087b22e8872
Charles <charles.b.jackson@protonmail.com>
2022-02-26 13:55:15 -0600

    Add script to guix pack for docker

--- pack-docker.sh
+++ pack-docker.sh
@@ -1,6 +1,7 @@
-guix pack -f docker \
-     --entry-point=/bin/issr-server \
-     -S /bin=bin \
-     -S /share=share \
-     bash \
-     -e '(load "guix.scm")'
+store=$(guix pack -f docker \
+             --entry-point=/bin/issr-server \
+             -S /bin=bin \
+             -S /share=share \
+             bash \
+             -e '(load "guix.scm")')
+cp $store ./issr-server-docker.tar.gz
